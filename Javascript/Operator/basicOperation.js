aae1f06608c90f5337187cb37ab7ae390fd755ed
Mete <Metterre@outlook.com.tr>
2022-08-29 21:45:46 +0300

    Updated

---
+++ Javascript/Operator/basicOperation.js
@@ -0,0 +1,38 @@
+var number1 = 5;
+var number2 = 4;
+
+console.log(number1 + number2);
+console.log(number1 - number2);
+console.log(number1 / number2);
+console.log(number1 * number2);
+console.log(number1--);
+console.log(number2++);
+console.log(number1 % number2);
+
+
+console.log(number1 == number2);
+console.log(number1 != number2);
+console.log(number1 < number2);
+console.log(number2 > number2);
+console.log(number1 >= number2);
+console.log(number1 <= number2);
+
+
+console.log(!true);
+console.log(true && true);
+console.log(false || true);
+
+
+console.log(number1 & number2);
+console.log(number1 | number2);
+console.log(number1 ^ number2);
+console.log(~number1);
+console.log(number1 << 3);
+console.log(number2 >> 3);
+
+
+console.log((number1 < number2) ? 100 : 200)
+
+
+
+
