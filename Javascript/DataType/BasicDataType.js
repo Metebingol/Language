89343e4696367d301fd19b5859fcce0fe4d6e2fb
Mete <Metterre@outlook.com.tr>
2022-08-29 21:24:44 +0300

    updated Java

---
+++ Javascript/DataType/BasicDataType.js
@@ -0,0 +1,16 @@
+function print(data) {
+    console.log(data);
+    return null;
+}
+var number1 = 5;    // Integer Declaration 
+var number2 = 5.2;  // Floating Point Declaration
+// There is no difference between floating point and integer in declaration
+print(number1);
+print(number2);
+print(typeof (number1));
+print(typeof (number2));
+var string1 = "Mete Bingol"; // String Declaration 
+print(string1);
+print(typeof (string1));
+var boolean1 = true; // Boolean Declaration
+print(boolean1);
---
+++ Javascript/DataType/conversionDataType.js
@@ -0,0 +1,9 @@
+var number1InString = "1"; // integer declaration in string
+var number1 = parseInt(number1InString);
+console.log(typeof (number1));
+var number2InString = "1.2"; // Float declartion in string
+var number2 = parseFloat(number2InString);
+console.log(typeof (number2));
+var number3 = 15;
+var number3InString = number3.toString();
+console.log(typeof (number3InString));
