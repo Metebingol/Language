68d2833f407fcdbe9ed81759d5abf83b4034f06f
Mete <Metterre@outlook.com.tr>
2022-09-05 08:41:22 +0300

    updated

---
+++ C#/test/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!\n");
+            string mete = Console.ReadLine();
+            Console.WriteLine("\n"+mete );
+            Console.WriteLine(Math.Max(4,3));
+        }
+    }
+}
